f9e9b749fb3c17afe1705c0a6e6246af56942a0e
Ryan <ryanpvoong@gmail.com>
2024-07-05 22:45:13 -0400

    testing dynamic url params. and fixed env

--- app/server.py
+++ app/server.py
@@ -1,9 +1,23 @@
+import requests
 from flask import Flask
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_Key = os.getenv('API_KEY')
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
   return "Hello World"
+
+@app.route('/<city>', methods=['GET'])
+def city_weather(city):
+  base_url = "https://api.openweathermap.org/data/2.5/weather?&appid="+API_Key+"&q="+city
+
+  weather_data = requests.get(base_url).json()
+  return weather_data
+
 
 app.run(host="0.0.0.0", port=9000, debug=True)
