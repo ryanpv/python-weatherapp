78a9b25a7a574e6edcc6bd279511648ac0eb3a09
Ryan <ryanpvoong@gmail.com>
2024-07-08 23:43:04 -0400

    reorganized redirect route. city param route redirects to another route while passing lat/lon data through session

--- app/server.py
+++ app/server.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_Key = os.getenv('API_KEY')
-SECRET_Key = os.getenv('SECRET_ENV')
+SECRET_Key = os.getenv('SECRET_KEY')
 
 app = Flask(__name__)
 app.secret_key = SECRET_Key
@@ -34,7 +34,7 @@ def coordinates():
     return jsonify(data)
   except ValueError as e:
     pprint({ "VALUE ERROR: ", e })
-    
+
 # TEMPS USING LAT / LON COORDINATES
 @app.route('/coordinates/list', methods=['GET'])
 def coordinatesSearch():
@@ -54,15 +54,10 @@ def coordinatesSearch():
     pprint({ "ERROR: ": e })
     return f"VALUE ERROR: { e }"
   
-
-
-    
-
-# RETURN ALL WEATHER DATA USING CITY QUERY
-@app.route('/geosearch')
-def home_page():
-  city_name = request.args.get('city')
-  base_url = "http://api.openweathermap.org/geo/1.0/direct?q="+city_name+"&appid="+API_Key
+# REDIRECT ROUTE - SEARCH LON / LAT USING CITY NAME 
+@app.route('/coordinates/list/<city>')
+def home_page(city):
+  base_url = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid="+API_Key
   result = requests.get(base_url).json()
   location = result[0]
   data = {
@@ -74,19 +69,31 @@ def home_page():
 
   session['data'] = data
   pprint(data)
-  return redirect(f"http://localhost:9000/city/toronto?lat={ data.get('lat') }&lon={ data.get('lon') }")
+  return redirect(f"http://localhost:9000/coordinates/data")
 
-@app.route('/city/<city>', methods=['GET'])
+@app.route('/coordinates/data')
+def redirectedData():
+  coordinates = session.get('data')
+  lat = coordinates.get('lat')
+  lon = coordinates.get('lon')
+
+  base_url = f"https://api.openweathermap.org/data/2.5/weather?lat={ lat }&lon={ lon }&appid={ API_Key }"
+  result = requests.get(base_url).json()
+  return result
+
+# DIRECT QUERY USING CITY NAME
+@app.route('/cities/<city>', methods=['GET'])
 def city_weather(city):
   lon = request.args.get('lon')
   lat = request.args.get('lat')
   location = session.get('data')
-  # base_url = "https://api.openweathermap.org/data/2.5/weather?&appid="+API_Key+"&q="+city
-  base_url = f"https://api.openweathermap.org/data/2.5/weather?lat={ lat }&lon={ lon }&appid={ API_Key }"
+  base_url = "https://api.openweathermap.org/data/2.5/weather?&appid="+API_Key+"&q="+city
+  # base_url = f"https://api.openweathermap.org/data/2.5/weather?lat={ lat }&lon={ lon }&appid={ API_Key }"
   result = requests.get(base_url).json()
 
   pprint(location)
   return jsonify(result)
+
 
 
 app.run(host="0.0.0.0", port=9000, debug=True)
