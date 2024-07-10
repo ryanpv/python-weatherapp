0639822d84d15148081e57f9f5ba376473a790dd
Ryan <ryanpvoong@gmail.com>
2024-07-10 14:22:17 -0400

    home route with list of routes

--- app/server.py
+++ app/server.py
@@ -13,6 +13,19 @@ app = Flask(__name__)
 app.secret_key = SECRET_Key
 
 ############## ROUTES ###############
+@app.route('/', methods=['GET'])
+def home_page():
+  data = {
+    "message": "Welcome to home page",
+    "route_1": "'/coordinates?city=<city>' to get lat/lon of a city's coordinates",
+    "route_2": "'/coordinates/list?lat=<lat_value>&lon=<lon_value>' to fetch weather using lat/lon values",
+    "route_3": "'/coordinates/list/<city>' fetches coordinates of city and redirects to route 4",
+    "route_4": "'/coordinates/data' retrieves coordinates from session and uses it to fetch data",
+    "route_5": "'/cities/<city>' fetches weather using just city name"
+  }
+
+  return data
+
 # ROUTE TO GET LAT / LON COORDINATES
 @app.route('/coordinates', methods=['GET'])
 def coordinates():
@@ -56,7 +69,7 @@ def coordinatesSearch():
   
 # REDIRECT ROUTE - SEARCH LON / LAT USING CITY NAME 
 @app.route('/coordinates/list/<city>')
-def home_page(city):
+def city_search(city):
   base_url = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid="+API_Key
   result = requests.get(base_url).json()
   location = result[0]
@@ -84,8 +97,6 @@ def redirectedData():
 # DIRECT QUERY USING CITY NAME
 @app.route('/cities/<city>', methods=['GET'])
 def city_weather(city):
-  lon = request.args.get('lon')
-  lat = request.args.get('lat')
   location = session.get('data')
   base_url = "https://api.openweathermap.org/data/2.5/weather?&appid="+API_Key+"&q="+city
   # base_url = f"https://api.openweathermap.org/data/2.5/weather?lat={ lat }&lon={ lon }&appid={ API_Key }"
